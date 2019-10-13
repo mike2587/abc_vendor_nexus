2a5339e184ce08bd3f0a840e4dafcb99d1be6d85
ezio84 <brabus84@gmail.com>
2019-10-13 18:43:31 +0200

    Remove all devices but taimen

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,18 @@
-aosp_devices=('blueline' 'bonito' 'crosshatch' 'dragon' 'marlin' 'sargo' 'taimen')
-caf_devices=('oneplus3')
+#aosp_devices=('blueline' 'bonito' 'crosshatch' 'dragon' 'marlin' 'sargo' 'taimen')
+#caf_devices=('oneplus3')
+aosp_devices=('taimen')
 
 function lunch_devices() {
     add_lunch_combo du_${device}-user
     add_lunch_combo du_${device}-userdebug
 }
 
-if [[ $( grep -i "caf" manifest/README.md) ]]; then
-    for device in ${caf_devices[@]}; do
-        lunch_devices
-    done
-else
+#if [[ $( grep -i "caf" manifest/README.md) ]]; then
+    #for device in ${caf_devices[@]}; do
+        #lunch_devices
+    #done
+#else
     for device in ${aosp_devices[@]}; do
         lunch_devices
     done
-fi
+#fi
